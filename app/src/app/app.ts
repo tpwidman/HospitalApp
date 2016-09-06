78ff1399478c40796cc4c8b69c40638930f24946
Taylor Widman <tpwidman@gmail.com>
2016-09-06 14:55:29 -0500

    added favicon

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,5 +1,10 @@
 import {App, events, register, routing, web} from 'platypus';
 import HomeViewControl from '../viewcontrols/home/home.vc';
+import AboutViewControl from '../viewcontrols/about/about.vc';
+import BaseViewControl from '../viewcontrols/base/base.vc';
+import ContactViewControl from '../viewcontrols/contact/contact.vc';
+import BlogViewControl from '../viewcontrols/blog/blog.vc'
+
 
 export default class MyApp extends App {
     constructor(router: routing.Router, config: web.IBrowserConfig) {
@@ -8,7 +13,11 @@ export default class MyApp extends App {
 		config.routingType = config.STATE;
 
         router.configure([
-            { pattern: '', view: HomeViewControl }
+            { pattern: '', view: HomeViewControl },
+            { pattern: 'about', view: AboutViewControl},
+            { pattern: 'base', view: BaseViewControl},
+            { pattern: 'contact', view: ContactViewControl},
+            { pattern: 'blog', view: BlogViewControl}
         ]);
     }
 

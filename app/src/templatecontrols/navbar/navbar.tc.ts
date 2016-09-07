ecb85489153bb7e878d00eaa3233cfaaf1493c35
Taylor Widman <tpwidman@gmail.com>
2016-09-06 21:57:48 -0500

    adding sample text

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -4,19 +4,31 @@ import AboutViewControl from '../../viewcontrols/about/about.vc';
 import ContactViewControl from '../../viewcontrols/contact/contact.vc';
 import BlogViewControl from '../../viewcontrols/blog/blog.vc';
 
-
-
-
 export default class NavbarTemplateControl extends ui.TemplateControl {
-    templateString: string = require('./navbar.tc.html');
+        title = 'Main';
+        templateUrl = 'viewcontrols/navbar/navbar.viewcontrol.html';
 
-    context: any = {
-        showNavbar: true,
-        homeView: HomeViewControl,
-        aboutView: AboutViewControl,
-        contactView: ContactViewControl,
-        blogView: BlogViewControl
-    }
-}
+        protected navbar: platui.Navbar = plat.acquire(platui.Navbar);
 
-register.control('navbar', NavbarTemplateControl);
+        initialize() {
+            this.initNavbar();
+        }
+        initNavbar() {
+            this.navbar.setLeft([{
+               content: '<span class="icon-arrow-left"></span><span>Back</span>',
+               action: () => {
+                   alert('back');
+               }
+            }]);
+
+            this.navbar.setCenter({
+               content: 'PlatypusUI'
+            });
+
+            this.navbar.setRight({
+               content: '<span>some icon</span>',
+               action: () => {
+                   alert('icon action');
+               }
+            });
+        }
--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -4,7 +4,9 @@ import BaseViewControl from '../base/base.vc';
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
+    context: any = {
+        carouselItems: [{ text: 'test0', bg: 'rgba(255,0,0,0.4)' }, { text: 'test1', bg: 'rgba(0,255,0,0.4)' }, { text: 'test2', bg: 'rgba(0,0,255,0.4)' }],
+    }
 }
 
 register.viewControl('home-vc', HomeViewControl);

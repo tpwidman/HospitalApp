f47434fd277deaa1e6e84a319d74bb317ceba2b4
Taylor Widman <tpwidman@gmail.com>
2016-09-07 01:38:30 -0500

    making some progress

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -5,7 +5,7 @@ export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
     context: any = {
-        carouselItems: [{ text: 'test0', bg: 'rgba(255,0,0,0.4)' }, { text: 'test1', bg: 'rgba(0,255,0,0.4)' }, { text: 'test2', bg: 'rgba(0,0,255,0.4)' }],
+        carouselItems: [{ text: 'carousel thing 1', bg: 'rgba(255,0,0,0.4)' }, { text: 'carousel thing 2', bg: 'rgba(0,255,0,0.4)' }, { text: 'carousel thing 3', bg: 'rgba(0,0,255,0.4)' }],
     }
 }
 

dd2a7fc547e99a4111f14a4cacb410f065e4f495
Taylor Widman <tpwidman@gmail.com>
2016-09-07 00:44:52 -0500

    navbar working

--- app/src/templatecontrols/navbar/navbar.tc.ts
+++ app/src/templatecontrols/navbar/navbar.tc.ts
@@ -1,34 +1,21 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui, web} from 'platypus';
+import BaseViewControl from '../../viewcontrols/base/base.vc';
 import HomeViewControl from '../../viewcontrols/home/home.vc';
 import AboutViewControl from '../../viewcontrols/about/about.vc';
 import ContactViewControl from '../../viewcontrols/contact/contact.vc';
 import BlogViewControl from '../../viewcontrols/blog/blog.vc';
 
+
 export default class NavbarTemplateControl extends ui.TemplateControl {
-        title = 'Main';
-        templateUrl = 'viewcontrols/navbar/navbar.viewcontrol.html';
+    templateString: string = require('./navbar.tc.html');
 
-        protected navbar: platui.Navbar = plat.acquire(platui.Navbar);
+    context: any = {
+        showNavbar: true,
+        homeView: HomeViewControl,
+        aboutView: AboutViewControl,
+        contactView: ContactViewControl,
+        blogView: BlogViewControl
+    };
+}
 
-        initialize() {
-            this.initNavbar();
-        }
-        initNavbar() {
-            this.navbar.setLeft([{
-               content: '<span class="icon-arrow-left"></span><span>Back</span>',
-               action: () => {
-                   alert('back');
-               }
-            }]);
-
-            this.navbar.setCenter({
-               content: 'PlatypusUI'
-            });
-
-            this.navbar.setRight({
-               content: '<span>some icon</span>',
-               action: () => {
-                   alert('icon action');
-               }
-            });
-        }
+register.control('navbar', NavbarTemplateControl);

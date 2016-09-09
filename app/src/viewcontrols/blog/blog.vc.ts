ef4a17004b0c1d6fadf9561e4bd203bdb5231714
Taylor Widman <tpwidman@gmail.com>
2016-09-09 10:28:27 -0500

    blog works

--- app/src/viewcontrols/blog/blog.vc.ts
+++ app/src/viewcontrols/blog/blog.vc.ts
@@ -4,7 +4,19 @@ import BaseViewControl from '../base/base.vc';
 export default class BlogViewControl extends BaseViewControl {
     templateString: string = require('./blog.vc.html');
 
-    context: any = {};
+    context: any = {
+        posts: [
+            {
+                title:'First post', content:'first content', author:'john'
+            },
+            {
+                title:'Second post', content:'second content', author:'jane'
+            },
+            {
+                title:'Third post', content:'third content', author:'jim'
+            }
+        ]
+    };
 }
 
 register.viewControl('blog-vc', BlogViewControl);

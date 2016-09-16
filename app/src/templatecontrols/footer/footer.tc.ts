ce8191ea6562579df40011b353a5233863201047
Taylor Widman <tpwidman@gmail.com>
2016-09-16 13:14:01 -0500

    fixed footer

--- app/src/templatecontrols/footer/footer.tc.ts
+++ app/src/templatecontrols/footer/footer.tc.ts
@@ -1,13 +1,22 @@
 import {controls, events, register, ui, web} from 'platypus';
+import HomeViewControl from '../../viewcontrols/home/home.vc';
+import AboutViewControl from '../../viewcontrols/about/about.vc';
+import ContactViewControl from '../../viewcontrols/contact/contact.vc';
+import BlogViewControl from '../../viewcontrols/blog/blog.vc';
 
 export default class FooterTemplateControl extends ui.TemplateControl {
     templateString: string = require('./footer.tc.html');
     context: any ={
-        modal1: false
+        modal1: false,
+        homeView: HomeViewControl,
+        aboutView: AboutViewControl,
+        contactView: ContactViewControl,
+        blogView: BlogViewControl
     };
     toggleModal(): void {
         this.context.modal1 = !this.context.modal1;
     };
+
 }
 
 register.control('footer', FooterTemplateControl);

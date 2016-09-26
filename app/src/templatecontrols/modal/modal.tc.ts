ce2ec2756462dabfaeb8f36ecba747cf4417a7ca
Taylor Widman <tpwidman@gmail.com>
2016-09-26 00:39:35 -0500

    trying to get this modal working

--- app/src/templatecontrols/modal/modal.tc.ts
+++ app/src/templatecontrols/modal/modal.tc.ts
@@ -1,10 +1,18 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui, web} from 'platypus';
+import BaseViewControl from '../../viewcontrols/base/base.vc';
+import HomeViewControl from '../../viewcontrols/home/home.vc';
+import AboutViewControl from '../../viewcontrols/about/about.vc';
+import ContactViewControl from '../../viewcontrols/contact/contact.vc';
+import BlogViewControl from '../../viewcontrols/blog/blog.vc';
 
 export default class ModalTemplateControl extends ui.TemplateControl {
     templateString: string = require('./modal.tc.html');
     context: any = {
-
-    }
+        homeView: HomeViewControl,
+        aboutView: AboutViewControl,
+        contactView: ContactViewControl,
+        blogView: BlogViewControl
+    };
 }
 
 register.control('mobileModal', ModalTemplateControl);
